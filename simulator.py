337b71b49bdddfd51f11ee9b2b512360ed63fc98
leynier <leynier41@gmail.com>
2020-03-28 01:42:17 -0400

    Implement simulation

--- simulator.py
+++ simulator.py
@@ -78,3 +78,60 @@ def arrival_ships(dike_rows=2, size_rows=6):
 def dike_cycle(ships):
     d = exponential_distribution
     return d(4) + sum(d(2) for _ in ships) + d(7) + d(len(ships) * 3/2)
+
+
+def simulation(number_of_dikes=5):
+    generator_ships = arrival_ships()
+    finished = False
+    time = 0
+    number_of_arrivals = 0
+    number_of_departures = 0
+    dikes = [[] for _ in range(number_of_dikes)]
+    arrivals = [[] for _ in range(number_of_dikes)]
+    departures = []
+    times = [INF for _ in range(number_of_dikes)]
+    try:
+        arrival = Ships(*next(generator_ships))
+    except StopIteration:
+        finished = True
+    while True:
+        min_time = min(times)
+        if not finished and arrival.time <= min_time:
+            time = arrival.time
+            number_of_arrivals += 1
+            dikes[0].append(arrival.ships)
+            arrivals[0].append(Ships(time, arrival.ships))
+            try:
+                arrival = Ships(*next(generator_ships))
+            except StopIteration:
+                finished = True
+            if len(dikes[0]) == 1:
+                departure_ships = dikes[0][0]
+                times[0] = time + dike_cycle(departure_ships)
+        else:
+            for p, ti in enumerate(times[:-1]):
+                if ti == min_time and len(dikes[p]) > 0:
+                    time = ti
+                    departure_ships = dikes[p].pop(0)
+                    dikes[p + 1].append(departure_ships)
+                    arrivals[p + 1].append(Ships(time, departure_ships))
+                    if dikes[p]:
+                        times[p] = time + dike_cycle(dikes[p])
+                    else:
+                        times[p] = INF
+                    if len(dikes[p + 1]) == 1:
+                        departure_ships = dikes[p + 1][0]
+                        times[p + 1] = time + dike_cycle(departure_ships)
+                    break
+            else:
+                if len(dikes[-1]) > 0:
+                    time = times[-1]
+                    number_of_departures += 1
+                    departure_ships = dikes[-1].pop(0)
+                    if dikes[-1]:
+                        times[-1] = time + dike_cycle(dikes[-1])
+                    else:
+                        times[-1] = INF
+                    departures.append(Ships(time, departure_ships))
+                else:
+                    return arrivals, departures
